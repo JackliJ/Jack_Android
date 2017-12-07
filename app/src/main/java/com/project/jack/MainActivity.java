fcb84c2d3d8066f193ef25b690bfdcdaf26c95ea
lijin <www.lijin@foxmail.com>
2017-12-07 17:40:55 +0800

    1.新建了项目，以后就在这上面做记录和提交了

--- app/src/main/java/com/project/jack/MainActivity.java
+++ app/src/main/java/com/project/jack/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //新建了项目  以后就在这上面做记录了
         setContentView(R.layout.activity_main);
     }
 }
